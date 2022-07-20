5dcf69b50e76cdc475cf01981b226cb8b490676b
ISSOtm <eldredhabert0@gmail.com>
2022-07-20 17:08:44 +0200

    Make build script executable

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 mkdir -p bin
 rgbgfx -cembedded -o bin/font.2bpp font.png
 rgbasm -ho bin/sram128.o sram128.asm
-rgblink -tdo bin/fail.gb bin/sram128.o && rgbfix -vp0xff bin/fail.gb
-rgblink -tdo bin/half_fail.gb bin/sram128.o && rgbfix -vp0xff -m0x1A -r5 bin/half_fail.gb
-rgblink -tdo bin/sram128.gb bin/sram128.o && rgbfix -vp0xff -m0x1A -r4 bin/sram128.gb
+rgblink -tdo bin/fail.gb bin/sram128.o && rgbfix -vp 0xff bin/fail.gb
+rgblink -tdo bin/half_fail.gb bin/sram128.o && rgbfix -vp 0xff -m 'MBC5+RAM' -r 5 bin/half_fail.gb
+rgblink -tdo bin/sram128.gb bin/sram128.o && rgbfix -vp 0xff -m 'MBC5+RAM' -r 4 bin/sram128.gb
 
